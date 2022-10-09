88269d11e4ec42d0d98d0821b56a31571dffae9c
MojMohammadian <115070390+MojMohammadian@users.noreply.github.com>
2022-10-09 20:44:43 +0330

    Add Start and Stop buttons

--- app/src/main/java/com/mohammadian/backgroundlocationtracker/MainActivity.kt
+++ app/src/main/java/com/mohammadian/backgroundlocationtracker/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.mohammadian.backgroundlocationtracker
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.mohammadian.backgroundlocationtracker.ui.theme.BackgroundLocationTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),0
+        )
         setContent {
             BackgroundLocationTrackerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +37,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Column(Modifier.fillMaxSize()) {
+                        Button(onClick = {
+                            Intent(
+                                applicationContext,
+                                LocationService::class.java
+                            ).apply {
+                                action = LocationService.ACTION_START
+                                startService(this)
+                            }
+                        }) {
+                            Text(text = "Start")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {
+                            Intent(
+                                applicationContext,
+                                LocationService::class.java
+                            ).apply {
+                                action = LocationService.ACTION_STOP
+                                startService(this)
+                            }
+                        }) {
+                            Text(text = "Stop")
+                        }
+
+                    }
                 }
             }
         }
